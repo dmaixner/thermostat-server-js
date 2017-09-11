73aa6221bf7f1cc53bfa726e615e1f3de50299af
David Maixner <dmaixner@gmail.com>
2017-09-11 09:20:46 +0200

    improve syntax

--- routes/temps.js
+++ routes/temps.js
@@ -32,7 +32,7 @@ var getRoomTemps = function (results, temps) {
 }
 
 router.get('/', function (req, res, next) {
-    let count = req.query.count ? req.query.count : config.numberOfResults;
+    let count = req.query.count || config.numberOfResults;
     axios.all(secrets.thingspeak.map((channel) => axios.get("https://api.thingspeak.com/channels/" + channel.channelId + "/feeds.json?api_key=" + channel.readKey + "&results=" + count)))
         .then((resultsArr) => resultsArr.reduce(getRoomTemps, {}))
         .catch((error) => res.status(500).send("!!! ERROR getting: " + error))
@@ -43,7 +43,7 @@ router.get('/', function (req, res, next) {
 router.get('/:roomId', function (req, res, next) {
     var roomChannel = secrets.thingspeak.filter((channel) => (channel.roomId === req.params.roomId));
     if (roomChannel.length === 1) {
-        let count = req.query.count ? req.query.count : config.numberOfResults;
+        let count = req.query.count || config.numberOfResults;
         axios.get("https://api.thingspeak.com/channels/" + roomChannel[0].channelId + "/feeds.json?api_key=" + roomChannel[0].readKey + "&results=" + count)
             .then((response) => getRoomTemps({}, response))
             .catch((error) => res.status(500).send("!!! ERROR getting: " + error))
